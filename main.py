37fcbbecfef03d75a5496f1d0d11048a1f552b56
MissMeridian <arietraingirl@gmail.com>
2026-03-16 10:33:22 -0500

    add occasional refresh of wsjr config and common.json

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import jrcontrol
 from netrunner import receiver 
-import asyncio, logging, coloredlogs
+import asyncio, logging, coloredlogs, time
 
 log = logging.getLogger(__name__)
 coloredlogs.install(level="DEBUG")
@@ -14,5 +14,14 @@ jrcontrol.load_star()
 receiver.FUNCTIONS["LF_LOAD"] = jrcontrol.load
 receiver.FUNCTIONS["LF_RUN"] = jrcontrol.run
 receiver.FUNCTIONS["LF_CANCEL"] = jrcontrol.cancel
+
+def refresh_configs(interval=1200): # every 20 minutes by default
+    while True:
+        time.sleep(interval)
+        log.info(f"Refreshing Jr configuration and common.json.")
+        jrcontrol.load_settings()
+        jrcontrol.load_star()
+
+receiver.threading.Thread(target=refresh_configs, daemon=True).start() # wretched but idgaf
 
 asyncio.run(receiver.main())
